8c694350a0490f7917e443995181474c6c486acf
yactouat <yactouat@hotmail.com>
2023-02-01 23:50:06 +0100

    getting the inside of a PubSub build message

--- blog.yactouat.com/pages/api/builds.ts
+++ blog.yactouat.com/pages/api/builds.ts
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { APIResponseType } from "pips_resources_definitions/dist/types";
-import { comesFromLegitPubSub } from "pips_resources_definitions/dist/behaviors";
+import {
+  comesFromLegitPubSub,
+  decodePubSubMessage,
+  sendJsonResponse,
+} from "pips_resources_definitions/dist/behaviors";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import getVercelBuilds from "@/lib/functions/get-vercel-builds";
@@ -10,39 +14,44 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<APIResponseType>
 ) {
+  if (req.method !== "GET" && req.method !== "POST") {
+    sendJsonResponse(res, 405, "Method not allowed");
+    return;
+  }
   if (req.method === "GET") {
     const builds = await getVercelBuilds(true);
-    res.status(200).json({
-      msg: `blog.yactouat.com ${builds.length} latest builds`,
-      data: builds,
-    });
-  }
-  if (req.method === "POST") {
-    // validating inbound payload from Google Cloud Pub/Sub
-    let pubSubEventWorkflowOk = comesFromLegitPubSub(
-      req,
-      process.env.PUBSUB_TOKEN_AUDIENCE as string,
-      process.env.PUBSUB_TOKEN_EMAIL as string
+    sendJsonResponse(
+      res,
+      200,
+      `blog.yactouat.com ${builds.length} latest builds`,
+      builds
     );
-    // posting the build request
-    if (pubSubEventWorkflowOk) {
-      try {
-        await postVercelBuild();
-      } catch (error) {
-        console.log("Pub/Sub event workflow Vercel build part KO");
-      }
+    return;
+  }
+  // POST request processing
+  // validating inbound payload from Google Cloud Pub/Sub
+  let pubSubEventWorkflowOk = comesFromLegitPubSub(
+    req,
+    process.env.PUBSUB_TOKEN_AUDIENCE as string,
+    process.env.PUBSUB_TOKEN_EMAIL as string
+  );
+  const message = decodePubSubMessage(req);
+  console.log("Pub/Sub event workflow message", message);
+  console.log("Pub/Sub event workflow message type", typeof message);
+  // posting the build request
+  if (pubSubEventWorkflowOk) {
+    try {
+      await postVercelBuild();
+    } catch (error) {
+      console.log("Pub/Sub event workflow Vercel build part KO");
     }
-    // responding to the inbound request so no reties will be attempted
-    if (pubSubEventWorkflowOk) {
-      console.log("Pub/Sub event workflow outcome OK");
-      res
-        .status(200)
-        .json({ msg: "blog.yactouat.com build triggered", data: null });
-    } else {
-      console.log("Pub/Sub event workflow outcome KO");
-      res
-        .status(422)
-        .json({ msg: "Pub/Sub event workflow outcome KO", data: null });
-    }
+  }
+  // responding to the inbound request so no reties will be attempted
+  if (pubSubEventWorkflowOk) {
+    console.log("Pub/Sub event workflow outcome OK");
+    sendJsonResponse(res, 200, "blog.yactouat.com build triggered");
+  } else {
+    console.log("Pub/Sub event workflow outcome KO");
+    sendJsonResponse(res, 422, "Pub/Sub event workflow outcome KO");
   }
 }
