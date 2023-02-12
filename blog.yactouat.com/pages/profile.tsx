ea1780227486f9a92d4e32cd4b51f92c70eafcf6
yactouat <yactouat@hotmail.com>
2023-02-12 18:43:03 +0100

    missed an erroring title

--- blog.yactouat.com/pages/profile.tsx
+++ blog.yactouat.com/pages/profile.tsx
@@ -130,8 +130,9 @@ export default function Profile() {
           setLoading(false);
         });
     } else {
-      setLoading(false);
       setErroring(true);
+      setLoading(false);
+      setTitle("...error");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userAuthToken]);
