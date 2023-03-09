d38339ea66640b219406b3b3307cba7ede5a1f8e
yactouat <yactouat@hotmail.com>
2023-03-09 21:44:54 +0100

    updating user tokens endpoint

--- blog.yactouat.com/pages/profile.tsx
+++ blog.yactouat.com/pages/profile.tsx
@@ -129,7 +129,7 @@ export default function Profile() {
   ): void => {
     setIsModalOpen(true);
     axios
-      .put(`${usersApiEndpoint}${urlUserId}/verify`, {
+      .put(`${usersApiEndpoint}${urlUserId}/process-token`, {
         email: urlEmail,
         [modtype]: urlToken,
       })
